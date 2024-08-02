6920c499d0afb78605deb4e2166df036dcf64d89
Awa Doumbia <awa80598@gmail.com>
2024-08-02 17:55:44 +0000

    home card detail menu footer shop integrer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Menu/Navbar';
 import Home from './components/Home/Home';
-
+import Shop from './components/Shop/Shop';
+import Shop_details from './components/Shop/Shop_details/Shop_details';
+import Contact from './components/Contact/Contact';
+import Cart from './components/Cart/Cart';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <Router>
@@ -11,8 +15,14 @@ function App() {
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/detail" element={<Shop_details />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/card" element={<Cart />} />
         </Routes>
       </main>
+      < Footer/>
+
     </Router>
   );
 }
--- src/components/Menu/Navbar.jsx
+++ src/components/Menu/Navbar.jsx
@@ -1,16 +1,10 @@
-// import React from 'react'
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-// import { IoClose, IoMenu } from "react-icons/io5";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
 <header >
-{/* <!-- Spinner Start --> */}
-        {/* <div id="spinner" className="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50  d-flex align-items-center justify-content-center">
-            <div className="spinner-grow text-primary" role="status"></div>
-        </div> */}
-        {/* <!-- Spinner End --> */}
 
 
         {/* <!-- Navbar start --> */}
@@ -28,6 +22,8 @@ export default function Navbar() {
                     </div>
                 </div>
             </div>
+
+            {/* //menu */}
             <div className="container px-0">
                 <nav className="navbar navbar-light bg-white navbar-expand-xl">
                     <a href="index.html" className="navbar-brand"><h1 className="text-primary display-6">Fruitables</h1></a>
@@ -35,27 +31,19 @@ export default function Navbar() {
                         <span className="fa fa-bars text-primary"></span>
                     </button>
                     <div className="collapse navbar-collapse bg-white" id="navbarCollapse">
-                        <div className="navbar-nav mx-auto">
-                            <a href="index.html" className="nav-item nav-link active">Home</a>
-                            <a href="shop.html" className="nav-item nav-link">Shop</a>
-                            <a href="shop-detail.html" className="nav-item nav-link">Shop Detail</a>
-                            <div className="nav-item dropdown">
-                                <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                                <div className="dropdown-menu m-0 bg-secondary rounded-0">
-                                    <a href="cart.html" className="dropdown-item">Cart</a>
-                                    <a href="chackout.html" className="dropdown-item">Chackout</a>
-                                    <a href="testimonial.html" className="dropdown-item">Testimonial</a>
-                                    <a href="404.html" className="dropdown-item">404 Page</a>
-                                </div>
-                            </div>
-                            <a href="contact.html" className="nav-item nav-link">Contact</a>
-                        </div>
+                        <nav className="navbar-nav mx-auto">
+                        <NavLink to="/" exact className="nav-item nav-link" activeClassName="active">Home</NavLink>
+                         <NavLink to="/shop" className="nav-item nav-link" activeClassName="active">Shop</NavLink>
+                          <NavLink to="/contact" className="nav-item nav-link" activeClassName="active">Contact</NavLink>
+                        </nav>
                         <div className="d-flex m-3 me-0">
                             <button className="btn-search btn border border-secondary btn-md-square rounded-circle bg-white me-4" data-bs-toggle="modal" data-bs-target="#searchModal"><i className="fas fa-search text-primary"></i></button>
+                            <NavLink to="/card" exact className="nav-item nav-link" activeClassName="active">
                             <a href="#" className="position-relative me-4 my-auto">
                                 <i className="fa fa-shopping-bag fa-2x"></i>
                                 <span className="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1" style={{top: -5, left: 15, height: 20,minWidth: '20px'}}>3</span>
                             </a>
+                            </NavLink>
                             <a href="#" className="my-auto">
                                 <i className="fas fa-user fa-2x"></i>
                             </a>
